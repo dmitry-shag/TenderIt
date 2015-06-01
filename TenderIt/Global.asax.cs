727713ab047f415469bd30973549b3432d995e41
StashBank <opavlovskyi@gmail.com>
2015-06-01 20:59:20 +0300

    add logger

--- TenderIt/Global.asax.cs
+++ TenderIt/Global.asax.cs
@@ -6,6 +6,7 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Ninject.Web.Common;
 
 namespace TenderIt
 {
@@ -14,14 +15,37 @@ namespace TenderIt
 
     public class MvcApplication : System.Web.HttpApplication
     {
+        private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
         protected void Application_Start()
         {
+            logger.Info("Application Start");
             AreaRegistration.RegisterAllAreas();
 
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        public void Init()
+        {
+            logger.Info("Application Init");
+        }
+
+        public void Dispose()
+        {
+            logger.Info("Application Dispose");
+        }
+
+        protected void Application_Error()
+        {
+            logger.Info("Application Error");
+        }
+
+
+        protected void Application_End()
+        {
+            logger.Info("Application End");
         }
     }
 }
